de0cf2c1d05bdcadd0e9790904f47f37c795226e
Octo Moon <octahedron00@gmail.com>
2020-12-14 03:59:43 +0900

    121404

---
+++ app_register.php
@@ -0,0 +1,16 @@
+<?php
+	$con = mysqli_connect("localhost","crud","qwerty2020","notebamboo");
+	$string = "INSERT INTO user (id, pw, nickname, email) VALUES ('"
+		. $_POST['id'] .
+		"','"
+		. $_POST['pwEnc'] .
+		"','"
+		. $_POST['nickname'] .
+		"','"
+		. $_POST['email'] .
+		"');";
+
+	echo $string;
+	//$result = mysqli_query($con, $string);
+	mysqli_close($con);
+ ?>
