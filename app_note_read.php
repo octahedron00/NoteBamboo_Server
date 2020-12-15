2b9cc442d9ad06f52c567111a2b9dc087dd92864
Octo Moon <octahedron00@gmail.com>
2020-12-16 05:08:27 +0900

    121605

---
+++ app_note_read.php
@@ -0,0 +1,14 @@
+<?php
+	$con = mysqli_connect("localhost","crud","qwerty2020","notebamboo");
+	$result = mysqli_query($con, "SELECT title, text, user, time FROM version where note=".$_POST['note'].";");
+
+	$response = array();
+	$response['success'] = false;
+	while($row = mysqli_fetch_array($result)){
+		$response = $row;
+		$response['success'] = true;
+	}
+
+	echo json_encode($response);
+	mysqli_close($con);
+ ?>
