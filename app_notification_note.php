7bfddf9447616c69d8a0af33c756ff8457c7ebe1
Octo Moon <octahedron00@gmail.com>
2020-12-17 01:22:12 +0900

    121701

--- app_notification_note.php
+++ app_notification_note.php
@@ -1,21 +1,30 @@
 <?php
 	$con = mysqli_connect("localhost","crud","qwerty2020","notebamboo");
 
-	$string = "INSERT INTO list (list, user, level, owner, name, AES_key, visible) VALUES ("
-		. $_POST['list'] .
-		","
-		. $_POST['user'] .
-		","
-		. $_POST['level'] .
-		",'"
-		. $_POST['owner'] .
-		"','"
-		. $_POST['name_Enc'] .
-		"','"
-		. $_POST['AES_key_Enc'] .
-		"',1);";
+	$result = mysqli_query($con,"SELECT no FROM list WHERE user=".$_POST['user']." AND list=".$_POST['list'].";");
 
-	echo $string;
+	$go = true;
+	while($row = mysqli_fetch_array($result)){
+		$go = false;
+	}
+
+	if($go){
+		$string = "INSERT INTO list (list, user, level, owner, name, AES_key, visible) VALUES ("
+			. $_POST['list'] .
+			","
+			. $_POST['user'] .
+			","
+			. $_POST['level'] .
+			",'"
+			. $_POST['owner'] .
+			"','"
+			. $_POST['name_Enc'] .
+			"','"
+			. $_POST['AES_key_Enc'] .
+			"',1);";
+	}
+
+	echo json_encode(array('success' => $go));
 	$result = mysqli_query($con, $string);
 	mysqli_close($con);
  ?>
