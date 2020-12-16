0a73a5fad6c86e0d5cb76065034f50b8cf6a178e
Octo Moon <octahedron00@gmail.com>
2020-12-17 03:18:58 +0900

    121703

--- app_list.php
+++ app_list.php
@@ -1,6 +1,6 @@
 <?php
 	$con = mysqli_connect("localhost","crud","qwerty2020","notebamboo");
-	$result = mysqli_query($con, "SELECT list, level, owner, AES_key, name, visible FROM list where user=".$_POST['user'].";");
+	$result = mysqli_query($con, "SELECT no, list, level, owner, AES_key, name, visible FROM list where user=".$_POST['user'].";");
 
 	$response = array();
 	while($row = mysqli_fetch_array($result)){
